73ef30dfab6d2d51703599f0a1aced5dbd02424b
Mithun Kannaa <mithunkannaa609@gmail.com>
2026-06-17 19:29:26 +0530

    6bil to 5bil changes

--- Model_dir/prepare_pretraining_data.py
+++ Model_dir/prepare_pretraining_data.py
@@ -12,17 +12,17 @@ os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"]="1"
 tok=Tokenizer.from_file("tokenizers_dir/tokenizer_49k_whitespace.json")
 
 #-----------------------------------------------------------------------------------------
-# ClimbMix 6 Billion Tokens Dataset
+# ClimbMix 5 Billion Tokens Dataset
 #-----------------------------------------------------------------------------------------
 
-def climbmix_6bil():
+def climbmix_5bil():
     '''
     Preprocesses the ClimbMix dataset and saves it as .npy files in the specified directory. 
     Each file contains a shard of the dataset with a maximum of 200 million tokens. 
     The function tokenizes the text data, adds special tokens for beginning and end of sequence, 
     and saves the tokenized data in batches to manage memory usage.
     '''
-    target=6_000_000_000
+    target=5_000_000_000
     count=0
     shard=1
 
@@ -33,7 +33,7 @@ def climbmix_6bil():
         streaming=True,
         split="train")
     
-    with tqdm(total=target, desc="ClimbMix 6bil", unit="Tokens", mininterval=0.1,miniters=1) as pbar:
+    with tqdm(total=target, desc="ClimbMix 5bil", unit="Tokens", mininterval=0.1,miniters=1) as pbar:
         for row in ds:
             tokenised=tok.encode(row["text"]).ids
             batch_count=len(tokenised)
@@ -43,14 +43,14 @@ def climbmix_6bil():
             pbar.update(batch_count+2)
             lst.extend([2]+tokenised+[3])
 
-            if count>=target//30:
+            if count>=target//25:
                 np.save(os.path.join(DATA_DIR,f"climbmix_{shard}.npy"),np.array(lst,dtype=np.uint16))
                 shard+=1
                 lst=[]
                 print(f"Saved shard {shard-1} with {count} tokens.")
                 count=0
             
-            if shard>30:
+            if shard>25:
                 break
 
 if __name__=="__main__":
@@ -58,5 +58,5 @@ if __name__=="__main__":
     print("Starting Preprocessing...")
 
     print("Downloading Climbmix...")
-    climbmix_6bil()
+    climbmix_5bil()
 
